9c66873868d5a6894037c4dbcd98fb697297c3a9
elysaans <elysa.nuzulul.2205356@students.um.ac.id>
2022-09-18 17:36:30 +0700

    lecture3

---
+++ bank.py
@@ -0,0 +1,10 @@
+answer = input("greeting : ").lower().strip()
+
+if 'hello' in answer:
+     print ("$0")
+elif "how you doing?" == answer:
+     print("$20")
+elif "what happening" == answer:
+     print("$100")
+else: 
+    print ("try again")         
